f7f6df2b12a6c9da2792b3f10e297dde045b3a64
hughwin <hugh.winchester@gmail.com>
2020-02-28 10:38:58 +0000

    Problems with server testing.

--- src/main/java/apit/uog/Main.java
+++ src/main/java/apit/uog/Main.java
@@ -1,14 +1,18 @@
 package main.java.apit.uog;
 
-import main.java.apit.uog.model.Dealer;
-import main.java.apit.uog.model.Deck;
-import main.java.apit.uog.view.AppView;
+import main.java.apit.uog.server.Server;
+
+import java.io.IOException;
+
 
 public class Main {
-    public static void main(String[] args) {
-        new AppView();
-        Dealer d = new Dealer(5);
-        d.dealCardsToPlayers();
+    public static void main(String[] args) throws IOException {
+      //  new AppView();
+       // Dealer d = new Dealer(5);
+     //   d.dealCardsToPlayers();
+        Server s = new Server();
+
+
 
 
     }
