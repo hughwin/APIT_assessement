42b4dd486004e80d3affe03a7c646f98cdf3f37e
hughwin <hugh.winchester@gmail.com>
2020-03-02 10:44:07 +0000

    Added HostAvailability class

--- src/main/java/apit/uog/Main.java
+++ src/main/java/apit/uog/Main.java
@@ -1,16 +1,25 @@
 package main.java.apit.uog;
 
+import main.java.apit.uog.model.GameLogic;
+import main.java.apit.uog.server.HostAvailability;
 import main.java.apit.uog.server.Server;
 
 import java.io.IOException;
 
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
+
+        if(!HostAvailability.hostAvailabilityCheck()){
+            Thread t = new Thread(new Server());
+            t.start();
+        }
+        Thread t2 = new Thread(new GameLogic());
+
       //  new AppView();
        // Dealer d = new Dealer(5);
      //   d.dealCardsToPlayers();
-        Server s = new Server();
+
 
 
 
--- src/main/java/apit/uog/model/GameLogic.java
+++ src/main/java/apit/uog/model/GameLogic.java
@@ -1,14 +1,22 @@
 package main.java.apit.uog.model;
 
+import main.java.apit.uog.view.AppView;
+
 import java.util.ArrayList;
 
-public class GameLogic {
+public class GameLogic implements Runnable {
 
     private int round;
+    private boolean roundComplete;
     private ArrayList<Player> activePlayer;
 
     public GameLogic(){
         activePlayer = new ArrayList<>();
+        new AppView();
     }
 
+    @Override
+    public void run() {
+        new GameLogic();
+    }
 }
